1a5b63ef4228a293147208bca03c1609fb330285
phu.ma <phu.ma>
2025-02-26 21:50:51 +0700

    1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { appConfig } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(appConfig.app.port);
+  const config = new DocumentBuilder()
+    .setTitle('Microservices API')
+    .setDescription('API documentation for microservices')
+    .setVersion('1.0')
+    .addTag('gateway')
+    .build();
+
+  // Tạo tài liệu Swagger cho API Gateway
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Kích hoạt Swagger UI tại /api-docs
+  SwaggerModule.setup('api-docs', app, document);
 }
 bootstrap();
